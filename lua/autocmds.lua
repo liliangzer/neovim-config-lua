fb10375003c197992468b378f6578993bce49411
ravenxrz <zhang.xingrui@foxmail.com>
2022-07-17 23:00:38 -0700

    disable auto-command about im-select and disable null-ls format plugin

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -17,14 +17,14 @@ autocmd("BufEnter", {
 
 -- 自动切换输入法，需要安装 im-select
 -- https://github.com/daipeihust/im-select
-autocmd("InsertLeave", {
-  group = myAutoGroup,
-  callback = require("utils.im-select").macInsertLeave,
-})
-autocmd("InsertEnter", {
-  group = myAutoGroup,
-  callback = require("utils.im-select").macInsertEnter,
-})
+-- autocmd("InsertLeave", {
+--   group = myAutoGroup,
+--   callback = require("utils.im-select").macInsertLeave,
+-- })
+-- autocmd("InsertEnter", {
+--   group = myAutoGroup,
+--   callback = require("utils.im-select").macInsertEnter,
+-- })
 
 -- 进入Terminal 自动进入插入模式
 autocmd("TermOpen", {
